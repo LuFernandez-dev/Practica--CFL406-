a680afe635a9fb662bfde8461e7b6e203e93d271
LuFernandez-dev <lucasfernandezlol2000@gmail.com>
2025-06-07 10:45:43 -0300

    secund commit

--- Condicional y bucle/calculadora.py
+++ Condicional y bucle/calculadora.py
@@ -5,6 +5,9 @@
 #4.Si elige esta opción(opcion 4) se interrumpirá la impresión del menú y el programa finalizará 
 #5.En caso de no introducir una opción válida, el programa informará de que no es correcta.
 
+#Importaciones
+import os
+
 #Ingreso de datos
 num_a = int(input("Ingrese un numero: "))
 num_b = int(input("Ingrese otro numero: "))
@@ -13,23 +16,28 @@ num_b = int(input("Ingrese otro numero: "))
 while True:
     #Opciones
     print("----------> CALCULADORA <----------\n")
-    print("1.SUMAR NUMEROS")
-    print("2.RESTAR NUMEROS")
-    print("3.MULTIPLICAR NUMEROS")
-    print("4.SALIR\n")
+    print("1.SUMAR")
+    print("2.RESTAR")
+    print("3.MULTIPLICAR")
+    print("4.DIVIDIR")
+    print("5.SALIR\n")
     option = int(input("Ingrese el numero de la opcion: "))
+    os.system("cls") #Limpia la pantalla
 
     #Operaciones
-    if option == 1:
-        print(f"La suma de los numeros es: {num_a + num_b}\n") #Suma
-    elif option == 2:
-        print(f"La resta de los numeros es: {num_a - num_b}\n") #Resta
-    elif option == 3:
-        print(f"La multiplicacion de los numeros es: {num_a * num_b}\n") #Multiplicacion
-    elif option == 4:
-        print("Fin del programa\n") #Salir del programa
-        break
-    #Error
-    else:
-        print("Ingrese una opcion valida\n")
-        
+    try:
+        if option == 1:
+            print(f"La suma de los numeros es: {num_a} + {num_b} = {num_a + num_b}\n") #Suma
+        elif option == 2:
+            print(f"La resta de los numeros es: {num_a} - {num_b} = {num_a - num_b}\n") #Resta
+        elif option == 3:
+            print(f"La multiplicacion de los numeros es: {num_a} * {num_b} = {num_a * num_b}\n") #Multiplicacion
+        elif option == 4:
+            print(f"La division de los numeros es: {num_a} / {num_b} = {num_a / num_b}\n") #Division
+        elif option == 5:
+            print("Fin del programa\n") 
+            break #Sale del bucle
+        else:
+            print("Opcion ingresada invalida intente de nuevo...") #Error en las opciones de la calculadora
+    except Exception as e:
+        print("Ha ocurrido un error", type(e).__name__) #Identifica errores no previsto
